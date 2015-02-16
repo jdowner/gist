d1d51ae1274d333ffd2bba57386076b094a09984
Joshua Downer <joshua.downer@gmail.com>
2015-02-16 13:12:13 -0500

    gist: documentation

--- gist/gist.py
+++ gist/gist.py
@@ -39,6 +39,13 @@ def elide(txt, width=terminal_width()):
     The string is elided to the specified width, which defaults to the width of
     the terminal.
 
+    Arguments:
+        txt: the string to potentially elide
+        width: the maximum permitted length of the string
+
+    Returns:
+        A string that is no longer than the specified width.
+
     """
     try:
         if len(txt) > width:
@@ -54,6 +61,13 @@ class authenticate(object):
     """
 
     def __init__(self, func, method='GET'):
+        """Create an authenticate object
+
+        Arguments:
+            func: a function to decorate
+            method: the method of the request to construct
+
+        """
         self.func = func
         self.owner = None
         self.instance = None
@@ -66,14 +80,32 @@ class authenticate(object):
 
     @classmethod
     def get(cls, func):
+        """Create an authenticate object with a GET method
+
+        Arguments:
+            func: a function to decorate
+
+        """
         return cls(func, method='GET')
 
     @classmethod
     def post(cls, func):
+        """Create an authenticate object with a POST method
+
+        Arguments:
+            func: a function to decorate
+
+        """
         return cls(func, method='POST')
 
     @classmethod
     def delete(cls, func):
+        """Create an authenticate object with a DELETE method
+
+        Arguments:
+            func: a function to decorate
+
+        """
         return cls(func, method='DELETE')
 
     def __get__(self, instance, owner):
@@ -110,15 +142,47 @@ class GistAPI(object):
     """
 
     def __init__(self, token):
+        """Create a GistAPI object
+
+        Arguments:
+            token: an authentication token
+
+        """
         self.token = token
 
     def send(self, request, stem=None):
+        """Prepare and send a request
+
+        Arguments:
+            request: a Request object that is not yet prepared
+            stem: a path to append to the root URL
+
+        Returns:
+            The response to the request
+
+        """
         if stem is not None:
             request.url = os.path.join(request.url, stem)
         return requests.Session().send(request.prepare())
 
     @authenticate.get
     def list(self, request):
+        """Prints a list of the users gists
+
+        An example of the print out from this function is,
+
+            bd99a4fd2677539d2706 + foo
+            41f6530ba1d9e86f6729 + bar
+            c78d925546e964b4b1df - baz
+
+        The first column is the identifier of the gist, the second columns is
+        either a '+' or '-' and corresponds to either a public or private gist,
+        the final column is the description of the gist, which may be empty.
+
+        Arguments:
+            request: an initial request object
+
+        """
         gists = self.send(request).json()
         for gist in gists:
             desc = gist['description']
@@ -128,6 +192,15 @@ class GistAPI(object):
 
     @authenticate.post
     def create(self, request, desc, files, public=False):
+        """Creates a gist
+
+        Arguments:
+            request: an initial request object
+            desc:    the gist description
+            files:   a list of files to add to the gist
+            public:  a flag to indicate whether the gist is public or not
+
+        """
         request.data = json.dumps({
                 "description": desc,
                 "public": public,
@@ -137,26 +210,62 @@ class GistAPI(object):
 
     @authenticate.delete
     def delete(self, request, id):
+        """Deletes a gist
+
+        Arguments:
+            request: an initial request object
+            id:      the gist identifier
+
+        """
         self.send(request, id)
 
     @authenticate.get
     def clone(self, request, id, name=''):
+        """Clone a gist
+
+        Arguments:
+            request: an initial request object
+            id:      the gist identifier
+            name:    the name to give the cloned repo
+
+        """
         response = self.send(request, id).json()
         os.system('git clone {} {}'.format(response['git_pull_url'], name))
 
     @authenticate.get
     def info(self, request, id):
+        """Print complete information about a gist
+
+        Arguments:
+            request: an initial request object
+            id:      the gist identifier
+
+        """
         gist = self.send(request, id).json()
         print(json.dumps(gist, indent=2))
 
     @authenticate.get
     def files(self, request, id):
+        """Print out files in a gist
+
+        Arguments:
+            request: an initial request object
+            id:      the gist identifier
+
+        """
         gist = self.send(request, id).json()
         for name in gist['files']:
             print(name)
 
     @authenticate.get
     def content(self, request, id):
+        """Print out the content of the file in a gist
+
+        Arguments:
+            request: an initial request object
+            id:      the gist identifier
+
+        """
         gist = self.send(request, id).json()
         for name, data in gist['files'].items():
             print('{}:\n'.format(name))
@@ -167,6 +276,21 @@ class GistAPI(object):
 
     @authenticate.get
     def archive(self, request, id):
+        """Create an archive of a gist
+
+        The files in the gist are downloaded and added to a compressed archive
+        (tarball). If the ID of the gist was c78d925546e964b4b1df, the resulting
+        archive would be,
+
+            c78d925546e964b4b1df.tar.gz
+
+        The archive is created in the directory where the command is invoked.
+
+        Arguments:
+            request: an initial request object
+            id:      the gist identifier
+
+        """
         gist = self.send(request, id).json()
 
         with tarfile.open('{}.tar.gz'.format(id), mode='w:gz') as archive:
