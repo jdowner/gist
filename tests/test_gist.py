fe44ce4a3dc5e5eee9a45be21788cc58c73ae668
Joshua Downer <joshua.downer@gmail.com>
2015-03-02 06:44:03 -0500

    gist: implementation of tests

--- tests/test_gist.py
+++ tests/test_gist.py
@@ -1,14 +1,109 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import unittest
 
+import json
 import responses
 
 import gist
 
 
 class TestGist(unittest.TestCase):
-    pass
+    @responses.activate
+    def test_list(self):
+        responses.add(responses.GET, 'https://api.github.com/gists',
+                body=json.dumps([
+                    {
+                        'id': 1,
+                        'description': 'test-desc-A',
+                        'public': True,
+                        },
+                    {
+                        'id': 2,
+                        'description': 'test-desc-\u212C',
+                        'public': False,
+                        },
+                    ]),
+                status=200,
+                )
+
+        gists = gist.GistAPI(token='foo').list()
+
+        gistA = gists[0]
+        gistB = gists[1]
+
+        self.assertEqual(gistA.id, 1)
+        self.assertEqual(gistA.desc, 'test-desc-A')
+        self.assertTrue(gistA.public)
+
+        self.assertEqual(gistB.id, 2)
+        self.assertEqual(gistB.desc, 'test-desc-\u212C')
+        self.assertFalse(gistB.public)
+
+    @responses.activate
+    def test_content(self):
+        responses.add(responses.GET, 'https://api.github.com/gists/1',
+                body=json.dumps({
+                    "files": {
+                        "file-A.txt": {
+                            "filename": "file-A.txt", 
+                            "content": "test-content-A", 
+                            },
+                        "file-B.txt": {
+                            "filename": "file-B.txt", 
+                            "content": "test-content-\u212C", 
+                            }
+                        }, 
+                    "description": "test-gist", 
+                    "public": True,
+                    "id": 1, 
+                    }),
+                status=200,
+                )
+
+        content = gist.GistAPI(token='foo').content('1')
+
+        self.assertEqual(len(content), 2)
+        self.assertTrue('file-A.txt' in content)
+        self.assertTrue('file-B.txt' in content)
+        self.assertEqual(content['file-A.txt'], 'test-content-A')
+        self.assertEqual(content['file-B.txt'], 'test-content-\u212C')
+
+    @responses.activate
+    def test_create(self):
+        def request_handler(request):
+            data = json.loads(request.body)
+            self.assertEqual(len(data['files']), 2)
+            self.assertTrue('test-file-A' in data['files'])
+
+            content = {k: v['content'] for k, v in data['files'].items()}
+
+            self.assertEqual(content['test-file-A'], 'test-content-A')
+            self.assertEqual(content['test-file-B'], 'test-content-\u212C')
+
+            status = 200
+            headers = {}
+            body = json.dumps({
+                'html_url': 'https://gist.github.com/gists/1'
+                })
+            return status, headers, body
+
+        responses.add_callback(
+                responses.POST,
+                'https://api.github.com/gists',
+                callback=request_handler,
+                content_type='application/json',
+                )
+
+        public = True
+        desc = 'test-desc'
+        files = {
+                'test-file-A': {'content': 'test-content-A'},
+                'test-file-B': {'content': 'test-content-\u212C'},
+                }
+
+        gist.GistAPI(token='foo').create(desc, files, public)
 
 
 if __name__ == "__main__":
