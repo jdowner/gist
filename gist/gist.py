874217fe8d10d6525d105589688481641dede748
Joshua Downer <joshua.downer@gmail.com>
2019-05-03 11:56:40 +0000

    gist: raise exception on bad response

--- gist/gist.py
+++ gist/gist.py
@@ -161,7 +161,12 @@ class GistAPI(object):
                                                            verify=None,
                                                            cert=None)
 
-        return self.session.send(prepped, **settings)
+        response = self.session.send(prepped, **settings)
+
+        if not response.ok:
+            response.raise_for_status()
+
+        return response
 
     def list(self):
         """Returns a list of the users gists as GistInfo objects
