0fb2d7b51cc11b2e4ee5d07a0323e06d1a3718e9
hugsy <hugsy@blah.cat>
2019-09-08 21:05:19 +0000

    travis: add support for python 3.7

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setuptools.setup(
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Topic :: Software Development',
             'Topic :: Software Development :: Version Control',
             'Topic :: Utilities',
